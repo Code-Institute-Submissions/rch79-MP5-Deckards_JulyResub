3b31e4b972cd39040ddb328d727ec4c10c73dd61
rch79 <rhorta@gmail.com>
2022-05-11 21:38:47 +0000

    Implement remove from bag functionality

--- bag_app/views.py
+++ bag_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.urls import reverse 
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 
@@ -27,3 +28,39 @@ def add_to_bag(request, book_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, book_id):
+    '''Add selected qty of specified product to shopping bag'''
+
+    quantity = int(request.POST.get('quantity'))
+
+    # stores contents of shopping bag in http session
+    # creates a new bag if one does not currently exists
+    # othewise adds to existing bag
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[book_id] = quantity  # changes the item qty accordingly
+    else:
+        bag.pop(book_id)  # deletes the item if qty is set to 0
+
+    request.session['bag'] = bag
+
+    # reverse returns URL in string format based on the URL name in urls.py
+    return redirect(reverse(view_bag))
+
+
+def remove_from_bag(request, book_id):
+    ''' Removes an item from the bag'''
+
+    try:
+
+        bag = request.session.get('bag', {})
+        bag.pop(book_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
