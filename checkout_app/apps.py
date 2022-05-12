5666b0cf874982605139cb2ad89830fddb2d3825
rch79 <rhorta@gmail.com>
2022-05-12 03:03:09 +0000

    Setup checkout admin and signals, and order form

--- checkout_app/apps.py
+++ checkout_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CheckoutAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout_app'
+
+    def ready(self):
+        import checkout_app.signals
