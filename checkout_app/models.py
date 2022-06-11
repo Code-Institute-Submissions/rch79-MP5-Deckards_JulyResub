0aeef024a8ed6712d4a376dd36ba60865dfea076
rch79 <rhorta@gmail.com>
2022-06-11 19:55:15 +0000

    Update Update country field and checkout page

--- checkout_app/models.py
+++ checkout_app/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
+from django_countries.fields import CountryField
+
 from books_app.models import Book
 
 # Create your models here.
@@ -21,7 +23,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=24, null=False, blank=False)
     county = models.CharField(max_length=80, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label="Country", null=False, blank=False)
     date = models.DateTimeField(auto_now=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
